b73ebd336a4cfd036a9293205988e7fb7da864bc
shadowsouls444 <angel.acuna.meza@gmail.com>
2025-08-20 20:16:01 -0500

    Cambios en el diseño

--- moviles/src/app/page/info-usuario/info-usuario.page.ts
+++ moviles/src/app/page/info-usuario/info-usuario.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonInput, IonLabel, IonItem } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-info-usuario',
   templateUrl: './info-usuario.page.html',
   styleUrls: ['./info-usuario.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonInput, IonLabel, IonItem]
 })
 export class InfoUsuarioPage implements OnInit {
 
