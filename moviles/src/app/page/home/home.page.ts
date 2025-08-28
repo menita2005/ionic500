d1249d71a5749177c2a57d48fe5b3968e10710e6
shadowsouls444 <angel.acuna.meza@gmail.com>
2025-08-27 19:42:44 -0500

    componentes usuarios hecho

--- moviles/src/app/page/home/home.page.ts
+++ moviles/src/app/page/home/home.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { ListaUsuarioComponent } from 'src/app/components/lista-usuario/lista-usuario.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ListaUsuarioComponent]
 })
 export class HomePage implements OnInit {
 
