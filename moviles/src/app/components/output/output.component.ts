a92bc04c68d63b840aeefc6fac9083293a6dd7c2
shadowsouls444 <angel.acuna.meza@gmail.com>
2025-11-05 20:22:00 -0500

    api

--- moviles/src/app/components/output/output.component.ts
+++ moviles/src/app/components/output/output.component.ts
@@ -29,21 +29,33 @@ export class OutputComponent implements OnInit {
       price: [0, [Validators.required, Validators.min(1), Validators.max(10000)]],
       description: ['', [Validators.required, Validators.minLength(20), Validators.maxLength(1000)]],
       category: ['', [Validators.required, Validators.minLength(3), Validators.pattern('^[a-zA-Z]+$')]],
+      rate:['',[Validators.required,Validators.min(1)]],
       image: ['']
     });
   }
 
   crear() {
-    if (this.formularioProducto.valid) {
-      const nuevoProducto: Producto = this.formularioProducto.value;
-      console.log('Producto creado:', nuevoProducto);
-      this.producto.emit(nuevoProducto);
-      this.formularioProducto.reset();
-    } else {
-      console.warn('Formulario inválido, revisa los campos');
-      this.formularioProducto.markAllAsTouched(); 0
-    }
+  if (this.formularioProducto.valid) {
+    const nuevoProducto: Producto = {
+      id: this.formularioProducto.get('id').value,
+      title: this.formularioProducto.get('title').value,
+      price: this.formularioProducto.get('price').value,
+      description: this.formularioProducto.get('description').value,
+      category: this.formularioProducto.get('category').value,
+      image: this.formularioProducto.get('image').value,
+      rating: {
+        rate: this.formularioProducto.get('rate').value,
+        count: 0,
+      }
+    };
+
+    this.producto.emit(nuevoProducto);
+    this.formularioProducto.reset();
+  } else {
+    alert("Datos incorrectos");
   }
+}
+
 
   
   campoInvalido(campo: string): boolean {
